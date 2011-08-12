f386be86e1ad59fd38a1340b0a2343fa2c22521b
milligramme <milligramme.cc@gmail.com>
2011-08-12 12:08:18 +0900

    add keyword input form

---
+++ views/localstore.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  function init() {
+    if (localStorage["keyword"]) {
+      $('#keyword').val(localStorage["keyword"]);
+    }
+  }
+
+  $('.localStore').keyup(function () {
+    localStorage[$(this).attr('id')] = $(this).val();
+  });
+  init();
+});
+var storekey = localStorage["keyword"].split(",");
+console.log(storekey);
